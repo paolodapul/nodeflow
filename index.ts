f8c4de6f7b2a27ea10e230b71ecd4727137f4872
Paolo Dapul <paolodapultech@gmail.com>
2024-05-03 16:46:48 +0800

    test DAG and BashOperator classes

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("hello world!");
+import { DAG } from "./src/DAG";
+import { BashOperator } from "./src/operators/bashOperator/BashOperator";
+
+const dag1 = new DAG("downloadRocketLaunches", new Date(), "");
+console.log(dag1);
+
+const bash = new BashOperator("", "ls", dag1);
+bash.runCommand();
